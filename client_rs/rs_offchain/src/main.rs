7b04408234eec9fad4ee1a74a5f34b52ce9e042b
xiyu1984 <shanellyy@126.com>
2022-11-17 23:19:10 +0800

    rs client

--- client_rs/rs_offchain/src/main.rs
+++ client_rs/rs_offchain/src/main.rs
@@ -8,5 +8,8 @@ async fn main() -> Result<(), anyhow::Error> {
     let address = SuiAddress::from_str("0x7f3b0d77188819024ff23080474e10dc18d575da")?;
     let objects = sui.read_api().get_objects_owned_by_address(address).await?;
     println!("{:?}", objects);
+
+    println!("{:#?}", sui.available_rpc_methods());
+
     Ok(())
 }
